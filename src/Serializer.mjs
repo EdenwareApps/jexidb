de0828ffae5a4c0a36215ce7fdf775d704b1d3c7
Edenware <efox.web@gmail.com>
2024-10-02 13:55:27 -0300

    Update Serializer.mjs

--- src/Serializer.mjs
+++ src/Serializer.mjs
@@ -2,138 +2,125 @@ import { EventEmitter } from 'events'
 import zlib from 'zlib'
 import v8 from 'v8'
 
-export default class Serializer extends EventEmitter {  
-  constructor(opts={}) {
+export default class Serializer extends EventEmitter {
+  constructor(opts = {}) {
     super()
     this.opts = Object.assign({}, opts)
     this.linebreak = Buffer.from([0x0A])
     this.delimiter = Buffer.from([0xFF, 0xFF, 0xFF, 0xFF])
-    this.defaultBuffer = Buffer.alloc(4096)  
+    this.defaultBuffer = Buffer.alloc(4096)
     this.brotliOptions = {
       params: {
         [zlib.constants.BROTLI_PARAM_QUALITY]: 4
       }
     }
   }
-  
-  async serialize(data, opts={}) {
-    let line
-    if(this.opts.v8) {
-      let compressed
-      line = v8.serialize(data)
-      if(this.opts.compress || opts.compress === true) {
-        await new Promise(resolve => {
-          zlib.brotliCompress(line, this.brotliOptions, (err, buffer) => {
-            if(!err) {
-              compressed = true
-              line = this.packLineBreaks(buffer, opts.linebreak !== false)
-            }
-            resolve()
-          })
-        })
-      }
-      if(!compressed && opts.linebreak !== false) {
-        line.copy(this.defaultBuffer)
-        this.linebreak.copy(this.defaultBuffer, line.length)
-        line = this.defaultBuffer.subarray(0, line.length + this.linebreak.length)
-      }
-    } else {
-      let compressed
-      line = JSON.stringify(data)
-      if(this.opts.compress || opts.compress === true) {
-        await new Promise(resolve => {
-          zlib.brotliCompress(line, this.brotliOptions, (err, buffer) => {
-            if(!err) {
-              compressed = true
-              line = this.packLineBreaks(buffer, opts.linebreak !== false)
-            }
-            resolve()
-          })
-        })
-      }
-      if(!compressed && opts.linebreak !== false) {
-        line += '\n'
-      }
-    }
-    return line
-  }  
 
-  async deserialize(data, opts={}) {
-    let line, removedLineBreak
-    if(this.opts.compress || opts.compress === true) {
+  async serialize(data, opts = {}) {
+    let line
+    let header = 0x00 // 1 byte de header
+    let compressed = false
+
+    if (this.opts.v8) {
+      header |= 0x02 // Indica V8
+      line = v8.serialize(data)
+    } else {
+      line = Buffer.from(JSON.stringify(data), 'utf-8')
+    }
+
+    if (this.opts.compress || opts.compress === true) {
       await new Promise(resolve => {
-        const n = this.unpackLineBreaks(data)
-        zlib.brotliDecompress(n, (err, buffer) => {
-          if(!err) {
-            removedLineBreak = true
-            data = buffer
+        zlib.brotliCompress(line, this.brotliOptions, (err, buffer) => {
+          if (!err) {
+            compressed = true
+            line = buffer
+          }
+          resolve()
+        })
+      })
+      header |= 0x01 // Indica compressão
+    }
+
+    line = this.packLineBreaks(line, opts.linebreak !== false)
+
+    // Prepara o buffer final com o header no início
+    const finalBuffer = Buffer.concat([Buffer.from([header]), line])
+
+    return finalBuffer
+  }
+
+  async deserialize(data, opts = {}) {
+    let header, isCompressed = false, isV8 = false
+    if (data[0] === 0x00 || data[0] === 0x01 || data[0] === 0x02 || data[0] === 0x03) { // has valid header
+      header = data[0]
+      isCompressed = (header & 0x01) === 0x01
+      isV8 = (header & 0x02) === 0x02
+      data = data.slice(1)
+    } else {
+      // presume it is not compressed and not V8
+      isCompressed = false
+      isV8 = false
+    }
+    let line = data
+    if (isCompressed) {
+      await new Promise(resolve => {
+        zlib.brotliDecompress(this.unpackLineBreaks(line), (err, buffer) => {
+          if (!err) {
+            line = buffer
           }
           resolve()
         })
       })
     }
-    if((this.opts.v8 && !opts.json) || opts.v8 === true) {
-      if(removedLineBreak !== true)  {
-        data = data.subarray(0, -1)
-      }
+
+    if (isV8) {
       try {
-        line = v8.deserialize(data)
-      } catch(e) {
-        if(opts.v8 === true) { // already retrying
-          throw new Error('Failed to deserialize V8 data')
-        }
-        opts.json = true
-        return await this.deserialize(data, opts)
+        return v8.deserialize(this.unpackLineBreaks(line))
+      } catch (e) {
+        throw new Error('Failed to deserialize V8 data')
       }
     } else {
-      if(Buffer.isBuffer(data)) {
-        data = data.toString('utf-8')
-      }
       try {
-        line = JSON.parse(data)
-      } catch(e) {
-        if(opts.json === true) {
-          throw new Error('Failed to deserialize JSON data')
-        }
-        opts.v8 = true
-        return await this.deserialize(data, opts)
+        return JSON.parse(line.toString('utf-8'))
+      } catch (e) {
+        throw new Error('Failed to deserialize JSON data')
       }
     }
-    return line
   }
 
+
   packLineBreaks(buffer, endWithLineBreak) {
-    if(buffer.length > (this.defaultBuffer.length + this.delimiter.length)) {
+    if (buffer.length > (this.defaultBuffer.length + this.delimiter.length)) {
       this.defaultBuffer = Buffer.alloc(buffer.length)
       return this.packLineBreaks(buffer, endWithLineBreak)
     }
-    let offset = 0
     const positions = []
     for (let i = 0; i < buffer.length; i++) {
       if (buffer[i] === 0x0A) {
         positions.push(i)
+        this.defaultBuffer[i] = 0xFF
       } else {
-        this.defaultBuffer[offset++] = buffer[i]
+        this.defaultBuffer[i] = buffer[i]
       }
     }
-    this.delimiter.copy(this.defaultBuffer, offset)
-    offset += this.delimiter.length
+    this.delimiter.copy(this.defaultBuffer, buffer.length)
+    let offset = buffer.length + this.delimiter.length
 
     const bytesWritten = this.defaultBuffer.write(JSON.stringify(positions), offset, 'utf-8')
-    const totalLength = offset + bytesWritten + (endWithLineBreak ? 1 : 0)    
-    if(totalLength > this.defaultBuffer.length) { // abort and retry if the buffer will overflown
+    const totalLength = offset + bytesWritten + (endWithLineBreak ? 1 : 0)
+    if (totalLength > this.defaultBuffer.length) { // predict overflowing buffer
       this.defaultBuffer = Buffer.alloc(totalLength)
       return this.packLineBreaks(buffer, endWithLineBreak)
     }
     offset += bytesWritten
     if (endWithLineBreak) {
       this.defaultBuffer[offset] = 0x0A
-    }  
+    }
     const result = Buffer.alloc(totalLength)
     this.defaultBuffer.copy(result, 0)
     return result
   }
-  
+
   unpackLineBreaks(buffer) {
     const delimiterIndex = buffer.lastIndexOf(this.delimiter)
     if (delimiterIndex === -1) return buffer
@@ -144,19 +131,10 @@ export default class Serializer extends EventEmitter {
       return buffer // not packed
     }
     if (positions.length === 0) return buffer.subarray(0, delimiterIndex)
-    let offset = delimiterIndex
-    let posIndex = positions.length - 1
-    for (let i = delimiterIndex - 1; i >= 0; i--) {
-      buffer[offset--] = buffer[i]
-      if (posIndex >= 0 && i === positions[posIndex]) { // Insert the line break if the position is found
-        buffer[offset--] = 0x0A
-        posIndex--
-      }
+    for (let i = 0; i < positions.length; i++) {
+      buffer[positions[i]] = 0x0A
     }
-    const start = offset + 1, end = delimiterIndex + positions.length + 1
-    const result = Buffer.alloc(end - start)
-    buffer.copy(result, 0, start)
-    return result
+    return buffer.slice(0, delimiterIndex)
   }
 
   async safeDeserialize(json) {
